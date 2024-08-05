eb16d26a88620c72f3d6ee22a752897c32be9449
DouglasSochC <3008474750101@ingenieria.usac.edu.gt>
2024-08-05 17:53:16 -0600

    Tarea 1 - Finalizada

--- Tarea1/script.js
+++ Tarea1/script.js
@@ -1,25 +1,43 @@
 // MIT License
-// Copyright (c) 2020 Luis Espino
 
-function reflex_agent(location, state){
-    if (state=="DIRTY") return "CLEAN";
-    else if (location=="A") return "RIGHT";
-    else if (location=="B") return "LEFT";
+function reflex_agent(location, state) {
+    if (state == "DIRTY") return "CLEAN";
+    else if (location == "A") return "RIGHT";
+    else if (location == "B") return "LEFT";
 }
 
-function test(states){
-       var location = states[0];		
-       var state = states[0] == "A" ? states[1] : states[2];
-       var action_result = reflex_agent(location, state);
-       document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
-       if (action_result == "CLEAN"){
-         if (location == "A") states[1] = "CLEAN";
-          else if (location == "B") states[2] = "CLEAN";
-       }
-       else if (action_result == "RIGHT") states[0] = "B";
-       else if (action_result == "LEFT") states[0] = "A";		
- setTimeout(function(){ test(states); }, 2000);
+function test(states) {
+    // Aleatoriamente ensuciar una habitación
+    if (Math.random() < 0.3) {  // 30% de probabilidad de ensuciar
+        var dirtyLocation = Math.random() < 0.5 ? 1 : 2;  // 50% A, 50% B
+        states[dirtyLocation] = "DIRTY";
+        document.getElementById("log").innerHTML += "<br>¡Se ensució la habitación " + (dirtyLocation === 1 ? "A" : "B") + "!";
+    }
+
+    var checkStates = states[3];
+    var location = states[0];
+    var state = states[0] == "A" ? states[1] : states[2];
+    var action_result = reflex_agent(location, state);
+
+    let check = `A: ${states[1]} - B: ${states[2]} - Location: ${states[0]} - Action: ${action_result}`;
+    if (!checkStates.includes(check)) {
+        checkStates.push(check);
+    }
+
+    var currentState = "A: " + states[1] + " | B: " + states[2] + " | Location: " + states[0] + " | Action: " + action_result;
+    document.getElementById("log").innerHTML += "<br>".concat(currentState);
+    if (action_result == "CLEAN") {
+        if (location == "A") states[1] = "CLEAN";
+        else if (location == "B") states[2] = "CLEAN";
+    }
+    else if (action_result == "RIGHT") states[0] = "B";
+    else if (action_result == "LEFT") states[0] = "A";
+    const timeoutId = setTimeout(function () { test(states); }, 100);
+    if (checkStates.length == 8) {
+        console.log("Estados obtenidos: ", checkStates)
+        clearTimeout(timeoutId);
+    }
 }
 
-var states = ["A","DIRTY","DIRTY"];
+var states = ["A", "DIRTY", "DIRTY", []];
 test(states);
